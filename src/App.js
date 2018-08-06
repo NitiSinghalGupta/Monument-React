ec15ca6984b4a6850739c3ae3760aa31ad00880f
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-06 16:39:55 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+{/* repeated components       */}
          <Navbar />
+{/* components for Blog.html page          */}
+
+{/* components for Index.html page */}
+{/* repeated components   */}
+         <Footer />
       </div>
     );
   }
