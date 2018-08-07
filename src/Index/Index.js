50150096197abe2bb938d4cef074514723e75c07
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-06 17:40:25 -0700

    refactoring react components for index.html page

---
+++ src/Index/Index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Archive from './Archive';
+import AboutUs from './AboutUs';
+import Gallery from './Gallery';
+import Contact from './Contact';
+
+const index = () => {
+  return (
+	<div>
+      <Header />
+      <Archive />
+      <AboutUs />
+      <Gallery />
+      <Contact />
+    </div>
+  )
+}
+
+export default index;
